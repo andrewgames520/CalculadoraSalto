e5de6688ba319c45d553f70894563848c6fdc1a6
Andrew Rodrigues <andrewmirinhorodrigues@gmail.com>
2026-05-05 20:00:25 -0300

    teste1

--- src/core/SaltoTatu.java
+++ src/core/SaltoTatu.java
@@ -52,7 +52,8 @@ public class SaltoTatu implements ActionListener {
         playBtn.setFocusable(false);
 
         // Cria as variáveis de saída
-        // Distância que o jogador precisa correr, tempo de corrida e velocidade média testeetetre
+        // Distância que o jogador precisa correr, tempo de corrida e velocidade média test
+        System.out.println("teste");
         playerDistanceDescription = "Distância a percorrer (m): ";
         finalDistanceOutput = createTextField(playerDistanceDescription, 40, 150, 350, 30, false, false);
 
